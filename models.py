cce361ea9c6615db6e3461db51239bb3ea36a222
Sebastian Bugge <vkbugge@hotmail.com>
2023-05-17 00:10:32 +0200

    implement distributed bellman-ford

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Node:
     id: int
@@ -9,6 +10,41 @@ class Node:
     sink: "Route" = None
     drain: "Route" = None
 
+    def process_routes(self, sink: "Route", drain: "Route") -> None:
+        if sink is not None and (
+            self.sink is None
+            or sink.cost < self.sink.cost
+            or (sink.source == self.sink.source)
+        ):
+            self.sink = sink
+        if drain is not None and (
+            self.drain is None
+            or drain.cost < self.drain.cost
+            or (drain.source == self.drain.source)
+        ):
+            self.drain = drain
+
+    def send_routes(self) -> None:
+        for connection in self.connections.values():
+            connected_node = (
+                connection.nodes[0]
+                if connection.nodes[0].id != self.id
+                else connection.nodes[1]
+            )
+
+            send_sink = (
+                Route(self.id, self.sink.cost, connection.cost)
+                if self.sink is not None
+                else None
+            )
+            send_drain = (
+                Route(self.id, self.drain.cost, connection.cost)
+                if self.drain is not None
+                else None
+            )
+            connected_node.process_routes(send_sink, send_drain)
+
+
 @dataclass
 class Connection:
     nodes: tuple[Node, Node]
@@ -16,15 +52,22 @@ class Connection:
 
     @property
     def cost(self) -> int:
-        return self.distance**2
+        return self.distance ** 2
 
     def __repr__(self) -> str:
         return f"Connection({self.nodes[0].id}, {self.nodes[1].id})"
 
-@dataclass(frozen=True)
+
+@dataclass
 class Route:
-    next_node: Node
-    dist: float
+    source: int
+    initial_cost: float
+    added_cost: float = 0
+
+    @property
+    def cost(self) -> float:
+        return self.initial_cost + self.added_cost
+
 
 @dataclass(frozen=True)
 class Point:
--- value_evaluator.py
+++ value_evaluator.py
@@ -2,103 +2,22 @@ from typing import Optional
 from models import Node, Route
 from dataclasses import dataclass
 
+
 def dfs_nodes(start: Node) -> dict[int, Node]:
     nodes: dict[int, Node] = {}
+
     def inner(node):
         if node.id in nodes:
             return
 
         nodes[node.id] = node
         for connection in node.connections.values():
-            connected_node = connection.nodes[0] if connection.nodes[0].id != node.id else connection.nodes[1]
+            connected_node = (
+                connection.nodes[0]
+                if connection.nodes[0].id != node.id
+                else connection.nodes[1]
+            )
             inner(connected_node)
+
     inner(start)
     return nodes
-    
-@dataclass
-class DijkstraNode:
-    cost: int
-    prev: int
-    node: Node
-
-def dijkstra(start: Node) -> dict[int, DijkstraNode]:
-    heap = [DijkstraNode(0, -1, start)]
-
-    result: dict[int, DijkstraNode] = {}
-    while node := pop_heap(heap):
-        if node.node.id in result:
-            continue
-        result[node.node.id] = node
-
-        for connection in node.node.connections.values():
-            if connection.distance > 6:
-                continue
-            connected_node = connection.nodes[0] if connection.nodes[0].id != node.node.id else connection.nodes[1]
-            new_dist = node.cost + connection.cost
-
-            if connected_node.id not in result or new_dist < result[connected_node.id].cost:
-                insert_heap(heap, DijkstraNode(new_dist, node.node.id, connected_node))
-
-    return result
-
-def update_distances(start: Node, end: Node) -> None:
-    nodes = dfs_nodes(start)
-    dijkstra_nodes = dijkstra(start)
-    for node in nodes.values():
-        dijkstra_node = dijkstra_nodes[node.id]
-        if dijkstra_node.prev == -1: continue
-        node.sink = Route(nodes[dijkstra_node.prev], dijkstra_node.cost)
-
-    dijkstra_nodes = dijkstra(end)
-    for node in nodes.values():
-        dijkstra_node = dijkstra_nodes[node.id]
-        if dijkstra_node.prev == -1: continue
-        node.drain = Route(nodes[dijkstra_node.prev], dijkstra_node.cost)
-
-def insert_heap(heap: list[DijkstraNode], node: DijkstraNode):
-    position = len(heap)
-    heap.append(node)
-
-    while position > 0:
-        parent_position = (position - 1) // 2
-        parent = heap[parent_position]
-
-        if node.cost < parent.cost:
-            heap[parent_position], heap[position] = node, parent
-            position = parent_position
-        else:
-            break
-
-def heapify(heap: list[DijkstraNode]):
-    if len(heap) == 0:
-        return
-
-    position = 0
-    node = heap[position]
-
-    while position < (len(heap) - 2) // 2:
-        left_position = position * 2 + 1
-        right_position = position * 2 + 2
-
-        if right_position >= len(heap):
-            candidate_position = left_position
-        else:
-            candidate_position = min(left_position, right_position, key=lambda x: heap[x].cost)
-
-        candidate = heap[candidate_position]
-        if candidate.cost < node.cost:
-            heap[position], heap[candidate_position] = heap[candidate_position], heap[position]
-            position = candidate_position
-        else:
-            break
-
-def pop_heap(nodes: list[DijkstraNode]) -> Optional[DijkstraNode]:
-    if len(nodes) == 0:
-        return None
-
-    node = nodes[0]
-    nodes[0] = nodes[-1]
-    nodes.pop()
-
-    heapify(nodes)
-    return node
